f00edcc0c2500a5be1896a2b9cfda1ccdfe6f570
Arthur Gomes <gomes.arthur2@gmail.com>
2017-09-02 22:08:21 -0400

    Week 1: Primeiro caso de teste e class inicial

---
+++ Coursera-TDD/Week1/src/main/src/main/StringCamelCase.java
@@ -0,0 +1,11 @@
+package src.main;
+
+public class StringCamelCase {
+
+	public static String converter(String string)
+	{
+		
+		return string;
+	}
+
+}
--- Coursera-TDD/Week1/src/test/TesteStringCamelCase.java
+++ Coursera-TDD/Week1/src/test/TesteStringCamelCase.java
@@ -2,13 +2,29 @@ package src.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import src.main.StringCamelCase;
 
 public class TesteStringCamelCase {
 
+	String stringInput;
+	String stringOutput;
+	
+	@Before
+	public void limpaStrings() {
+		stringInput = "";
+		stringOutput = "";
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void stringSimplesValida() {
+		stringInput = "SimplesString";
+		
+		stringOutput = StringCamelCase.converter(stringInput);
+		
+		assertFalse(stringOutput.isEmpty());
 	}
 
 }
